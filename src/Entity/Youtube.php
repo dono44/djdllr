2fbd0dcd2b1cca984fd6816a3542d31340f719f2
dono44 <don00765@gmail.com>
2020-12-01 01:43:50 +0100

    suit tuto debur chapitre 3 sur la db

---
+++ src/Entity/Youtube.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\YoutubeRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=YoutubeRepository::class)
+ */
+class Youtube
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $titre;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $descristion;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(string $titre): self
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    public function getDescristion(): ?string
+    {
+        return $this->descristion;
+    }
+
+    public function setDescristion(?string $descristion): self
+    {
+        $this->descristion = $descristion;
+
+        return $this;
+    }
+}
